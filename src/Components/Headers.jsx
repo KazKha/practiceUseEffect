81e5e77f49986d49209ee9a610817638b11f244a
kazim <kazkha@gmail.com>
2023-06-28 17:03:55 +0530

    usereffect listing

---
+++ src/Components/Headers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Headers() {
+  return (
+    <div className='header-sections'>
+        <h3>UseEffets Hooks </h3>
+    </div>
+  )
+}
+
+export default Headers
